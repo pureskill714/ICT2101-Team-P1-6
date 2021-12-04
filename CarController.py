982d7f60e821ccc30cded96e185e86daf95cf43d
Don-Whis <2000522@sit.singaporetech.edu.sg>
2021-12-04 16:17:57 +0800

    Revert "Merge branch 'gamepage' into dev"
    This reverts commit 9e76d056d2860ae01a040e130fd84964a398617b, reversing
    changes made to a98123b7be4f61227652688f4637f4aa45a0f2c0.

--- CarController.py
+++ CarController.py
@@ -1,7 +1,4 @@
 from Car import Car
-import serial
-import time
-
 class CarController(Car):
     def __init__(self,name,pwd):
         super().__init__(name,pwd)
@@ -11,61 +8,11 @@ class CarController(Car):
     input2: pwd;
     output: boolean;
     """
-    def bluetoothSetUp(self):
-        ser = serial.Serial()
-        ser.port = "COM15"
-        ser.baudrate = 9600
-        ser.bytesize = serial.EIGHTBITS  # number of bits per bytes
-        ser.parity = serial.PARITY_NONE  # set parity check: no parity
-        ser.stopbits = serial.STOPBITS_ONE  # number of stop bits
-        # ser.timeout = None          #block read
-        ser.timeout = 1  # non-block read
-        # ser.timeout = 2              #timeout block read
-        ser.xonxoff = False  # disable software flow control
-        ser.rtscts = False  # disable hardware (RTS/CTS) flow control
-        ser.dsrdtr = False  # disable hardware (DSR/DTR) flow control
-        ser.writeTimeout = 2  # timeout for write
-        return ser
-
-    def openConnection(self):
-        ser = self.bluetoothSetUp()
-        try:
-            ser.open()
-            return True, ser
-        except:
-            return False, "Open connection fail"
-
-
-    def testConnection(self,ser):
-        if ser[0]:
-            if ser[1].isOpen():
-                return True,ser[1]
-            return False, "connection not open"
-        return False, ser[1]
-
-    def closeConnection(self):
-        ser = self.bluetoothSetUp()
-        ser.close()
-
-    def sendCommand(self,ser,cmd):
-        try:
-            ser.flushInput()  # flush input buffer, discarding all its contents
-            ser.flushOutput()  # flush output buffer, aborting current output
-            # write data
-            ser.write(str.encode(cmd))
-            print(f"write data: {cmd}")
-
-            time.sleep(1)  # give the serial port sometime to receive the data
-
-            response = ser.readline()
-
-            return True, response.decode('UTF-8')
-
-        except Exception as e1:
-            # msg[0] = False
-            exception = "Exception happens: " + str(e1)
-            return False,exception
-
+    def connection(self):
+        if self.carName == "1001": #dummy testing
+            return True
+        else:
+            return False
     '''
     use to set parameter of the car, data retrieve from adpator class
     input1: speed;
@@ -76,19 +23,10 @@ class CarController(Car):
     '''
     def setCarStat(self,speed,dist,status,state):
         Car.setSpeed(self,speed)
-        Car.setDist(self,dist)
         Car.setStatus(self,status)
+        Car.setDist(self,dist)
         Car.setState(self,state)
 
     def getCarStat(self):
-        stat = {}
-        stat["status"] = Car.getStatus(self)
-        stat["state"] = Car.getState(self)
-        stat["dist"] = Car.getDist(self)
-        stat["speed"] = Car.getSpeed(self)
-        # stat = [Car.getStatus(self),Car.getState(self),Car.getDist(self),Car.getSpeed(self)]
+        stat = [Car.getSpeed(self), Car.getStatus(self,), Car.getDist(self,),Car.getState(self)]
         return stat
-# car = CarController("sasas","asasas")
-# car.setCarStat("3m/h", "10.7m", "Connected","Idle")
-# stat = car.getCarStat()
-# print(stat)
--- WebPortal.py
+++ WebPortal.py
@@ -1,11 +1,7 @@
-
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template
 from CarController import CarController
-import serial
-import time
-
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 #cmd to start project
 #set FLASK_APP=WebPortal
 #flask run
@@ -15,51 +11,29 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 def index():
     return render_template('index.html')
 
+
+
 #instantiate new car
 car=CarController("1001","123456")
-conn = car.openConnection()
+
 #main page
 #calling connection() from carCOntroller before redirect
 @app.route('/main')
 def main():
-    if conn[0]:
-        flash("Connect successfully")
+    if car.connection():
         car.setCarStat(0,0,"connected","idle")
         data= car.getCarStat()
-        return render_template('main.html',data=data) #data = what u want to pass to html page eg. see main.html
+        return render_template('main.html', data=data) #data = what u want to pass to html page eg. see main.html
     else:
-        print(conn[1])
-        flash("Connection fail")
         return render_template('index.html')
 
-@app.route('/gamepage', methods=['GET', 'POST'])
-def game():
-    if request.method == 'POST':
-        cmd = request.form['cmdList']
-    car.setCarStat("10m/h","12m","Connected","Idle")
-    data = car.getCarStat()
-    data["map"] = [[1,1,0],[1,0,0],[0,0,1]]
-    data["start"] = [0,2]
-    data["end"] = [2,0]
-    # print(data["speed"])
-    return render_template('gamepage.html',data=data)
+@app.route('/selectlevel')
+def selectlevel():
+    return render_template('selectlevel.html')
 
-#background process happening without any refreshing
-@app.route('/sendcommand')
-def background_process_test():
-    cmd = request.args.get('cmd')
-    print(f"cmd received={cmd}")
-    connected = car.testConnection(conn)
-    if connected[0]:
-        result = car.sendCommand(connected[1],cmd)
-    # flash(result)
-        print(result[1])
-    else:
-        print(connected[1])
-    # if(result[0]):
-    #     flash("success"+result[1])
-    # else:
-    #     flash("failed"+result[1])
-    return ("nothing")
+@app.route('/Configlevelpage')
+def Configlevelpage():
+    return render_template('Configlevelpage.html')
+
 if __name__ =="__main__":
     app.run(debug=True)
